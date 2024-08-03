e3cb27503718d7b914bcf53829461d06983fd4c5
Emefiele-Konyeri Awinrin <69255475+Joshaw-k@users.noreply.github.com>
2024-08-03 15:17:08 +0100

    Update page.js

--- app/wallet/page.js
+++ app/wallet/page.js
@@ -99,7 +99,7 @@ export default function Home() {
       {
         connections: [
           {
-            channel_id: "1269123107843473470",
+            channel_id: "1269250241790742659",
           },
         ],
       }
