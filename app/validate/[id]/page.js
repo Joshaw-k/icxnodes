19267e1a6602f54d5641579f4261c74a8a42de55
Koniesjay <jkonyeri@gmail.com>
2025-02-17 12:27:35 +0100

    edit: duration, size and awx bonus amplifiers

--- app/validate/[id]/page.js
+++ app/validate/[id]/page.js
@@ -80,7 +80,10 @@ export default function Home() {
           </div>
           <div className="text-center grid gap-y-3">
             <h1 className="text-white text-3xl">Wallet Connection Error!!!</h1>
-            <p className="text-[#4B4F58]">Ensure you submit an active wallet</p>
+            <p className="text-[#4B4F58]">
+              Your wallet does not meet the minimum activity requirement. <br />
+              Please try again with a different wallet.
+            </p>
             <Link
               href="/"
               className="text-center px-4 py-2 text-white bg-[#BC3FEA] w-60 block rounded-md font-semibold mx-auto"
